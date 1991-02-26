c0bd141464b914f003a033af9b09b9edf64ca0ba
BillBenon <61540696+BillBenon@users.noreply.github.com>
1991-02-26 14:00:00 +0100

    intro: functions in python

---
+++ functions.py
@@ -0,0 +1,47 @@
+def my_func(param1="default"):
+    """
+    THIS IS THE DOCSTRING
+    """
+    print("my first python function! {}".format(param1))
+
+
+my_func()
+
+
+def hello():
+    return "hello"
+
+
+result = hello()
+print(result)
+
+
+def addNum(num1, num2):
+    if type(num1) == type(num2) == type(10):
+        return num1 + num2
+    else:
+        return "Sorry, I need integers"
+
+
+result = addNum(2, 3)
+print(type(result))
+
+# Lambda Expression
+
+# Filter
+mylist = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# def even_bool(num):
+#     return num % 2 == 0
+#
+# evens = filter(even_bool, mylist)
+# print(list(evens))
+
+evens = filter(lambda num: num % 2 == 0, mylist)
+print(list(evens))
+
+tweet = "Go sports! #Sports"
+result = tweet.split('#')[1]
+print(result)
+
+print('x' in [1, 2, 3, 'x'])
