cf40aa2e0ba3572ce9dd90dfbbf1cd157ec8d24f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-10-23 14:00:00 +0100

    intro: inheritance and special methods in java

--- oop.py
+++ oop.py
@@ -27,3 +27,55 @@ class Circle:
 myc = Circle(3)
 myc.set_radius(999)
 print(myc.area())
+
+
+# INHERITANCE
+class Animal:
+    def __init__(self):
+        print("ANIMAL CREATED")
+
+    def whoAmI(self):
+        print("ANIMAL")
+
+    def eat(self):
+        print('EATING')
+
+
+class Dog(Animal):
+    def __init__(self):
+        # Animal.__init__(self)
+        print("DOG CREATED")
+
+    def bark(self):
+        print("WOOF")
+
+    def eat(self):
+        print("DOG EATING")
+
+
+myd = Dog()
+myd.whoAmI()
+myd.eat()
+myd.bark()
+
+# SPECIAL METHODS
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "Title {}, Author: {}, Pages: {}".format(self.title, self.author, self.pages)
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("a book is destroyed")
+
+
+b = Book("Python", "jose", 200)
+print(b)
+print(len(b))
+del b
