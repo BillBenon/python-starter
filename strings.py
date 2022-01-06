e684b7bd700464f4acec2820d7b7265d530e4d0e
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-01-06 14:00:00 +0200

    strings intro in python

---
+++ strings.py
@@ -0,0 +1,11 @@
+mystring = "bill benon"
+# print(mystring[1:])
+# print(mystring[:3])
+# print(mystring[1:5])
+# print(mystring[::2])
+print(mystring.capitalize())
+print(mystring.split())
+
+y = "Insert one string here: {} and another string here: {}".format("INSERT ME!", "SECOND ONE")
+x = "String one is: {} and string two is: {}".format("dog", "cat")
+print(x)
