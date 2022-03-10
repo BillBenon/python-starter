9cc61655a1a10112d33911d216e9b45bfab0d3c5
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-03-10 11:26:06 +0200

    intro: tuples, sets, booleans

---
+++ tuples,sets,andbooleans.py
@@ -0,0 +1,21 @@
+# Tuples
+t = (1, 2, 3)
+print(t[0])
+t = ('a', True, 123)
+print(t)
+# t[0] = 'NEW'      this leads to an error because tuples are immutable, they don't support reassignment
+
+# Sets
+x = set()
+x.add(1)
+x.add(2)
+x.add(0.1)
+x.add(4)
+x.add(4)
+x.add(4)
+x.add('bill')
+print(x)
+
+# Booleans
+True
+False
